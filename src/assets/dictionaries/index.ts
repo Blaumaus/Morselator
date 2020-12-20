4549a124319849d4b2fbe62c56015bffcb41a13b
Blue Mouse <35810911+Blaumaus@users.noreply.github.com>
2020-12-20 23:21:07 +0200

    Added russian and ukrainian

--- src/assets/dictionaries/index.ts
+++ src/assets/dictionaries/index.ts
@@ -1,7 +1,11 @@
 import en from './en.json'
+import ru from './ru.json'
+import ua from './ua.json'
 
 const data: object[] = [
-    { name: en.name, code: en.code, data: en.data }
+    { name: en.name, code: en.code, data: en.data },
+    { name: ru.name, code: ru.code, data: ru.data },
+    { name: ua.name, code: ua.code, data: ua.data },
 ]
 
 export {
