3a634d0d16bad9e21a1c0e8d2caa49b54f45f5a8
Blue Mouse <35810911+Blaumaus@users.noreply.github.com>
2020-12-21 23:17:05 +0200

    Added some types checks

--- src/assets/dictionaries/index.ts
+++ src/assets/dictionaries/index.ts
@@ -2,7 +2,13 @@ import en from './en.json'
 import ru from './ru.json'
 import ua from './ua.json'
 
-const data: object[] = [
+interface JSONLang {
+    name: string
+    code: string
+    data: object
+}
+
+const data: JSONLang[] = [
     { name: en.name, code: en.code, data: en.data },
     { name: ru.name, code: ru.code, data: ru.data },
     { name: ua.name, code: ua.code, data: ua.data },
