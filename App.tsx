b6fba0eaf8a5b0fb4299670d0f2fc83e4ca3d59c
Blue Mouse <35810911+Blaumaus@users.noreply.github.com>
2020-12-24 22:41:46 +0200

    Fixed navigation header wrong colour and flickering

--- App.tsx
+++ App.tsx
@@ -3,29 +3,45 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { Provider } from 'react-redux'
 import store from './src/redux/store'
+import { connect } from 'react-redux'
+import { getThemeParam } from './src/themes'
 
 import Home from './src/components/Home/Home'
 import SelectLanguage from './src/components/Home/SelectLanguage'
+import Settings from './src/components/Settings'
 
-const HomeStack = createStackNavigator()
+const Stack = createStackNavigator()
 
-const App = () => {
-    return (
-        <Provider store={store}>
-            <NavigationContainer>
-                <HomeStack.Navigator>
-                    <HomeStack.Screen
-                        name="Home"
-                        component={Home}
-                        options={{ headerShown: false }} />
-                    <HomeStack.Screen
-                        name="Home.SelectLanguage"
-                        component={SelectLanguage}
-                        options={{ headerTitle: 'Select language' }} />
-                </HomeStack.Navigator>
-            </NavigationContainer>
-        </Provider>
-    )
-}
+const StackNavigator = ({ theme }) => (
+    <Stack.Navigator screenOptions={{ 
+        animationEnabled: false,
+        headerStyle: { backgroundColor: getThemeParam('headerBackgroundColor', theme) },
+        headerTitleStyle: { color: getThemeParam('descTextColor', theme) },
+        headerTintColor: getThemeParam('descTextColor', theme)
+        }}>
+        <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ headerShown: false }} />
+        
+        <Stack.Screen
+            name="Home.SelectLanguage"
+            component={SelectLanguage}
+            options={{ headerTitle: 'Select language' }} />
 
-export default App
+        <Stack.Screen 
+            name="Settings"
+            component={Settings}
+            options={{ headerTitle: 'Settings' }} />
+    </Stack.Navigator>
+)
+
+const ConnectedNavigator = connect(state => ({ theme: state.theme.theme }))(StackNavigator)
+
+export default () => (
+    <Provider store={store}>
+        <NavigationContainer>
+            <ConnectedNavigator />
+        </NavigationContainer>
+    </Provider>
+)
